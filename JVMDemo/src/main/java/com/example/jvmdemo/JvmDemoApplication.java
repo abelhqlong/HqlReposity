52c6b1e97bf90c6147892e6ec8960d976cbf93e5
abel <946233586@qq.com>
2022-06-20 13:50:45 +0800

    1.测试提交

---
+++ JVMDemo/src/main/java/com/example/jvmdemo/JvmDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.jvmdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JvmDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JvmDemoApplication.class, args);
+    }
+
+}
---
+++ JVMDemo/src/test/java/com/example/jvmdemo/JvmDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jvmdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JvmDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
