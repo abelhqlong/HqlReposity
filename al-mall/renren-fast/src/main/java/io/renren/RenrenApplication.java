8c7937d085496e2054f88b14495ec9e2f7be71c2
abel.huang <Abel.huang@zkteco.com>
2022-09-15 17:06:59 +0800

    1.文件修改

--- al-mall/renren-fast/src/main/java/io/renren/RenrenApplication.java
+++ al-mall/renren-fast/src/main/java/io/renren/RenrenApplication.java
@@ -12,6 +12,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
+//@EnableDiscoveryClient
 @SpringBootApplication
 public class RenrenApplication {
 
